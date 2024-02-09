b2118b7fdfff47e58ef62756a2ab1566078a4ae2
Alexey Korotin <sx12fx@gmail.com>
2024-02-09 20:00:34 +0300

    fix: add missing user id to the cash flows

--- backend/src/main/java/org/itmo/practice/pfm/domain/cashflow/IncomeSource.java
+++ backend/src/main/java/org/itmo/practice/pfm/domain/cashflow/IncomeSource.java
@@ -2,21 +2,32 @@ package org.itmo.practice.pfm.domain.cashflow;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.itmo.practice.pfm.domain.funds.Money;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = {"id"})
 public class IncomeSource {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    private String userId;
 
     @Embedded
     private CashFlowName name;
 
     @Embedded
     private Money monthlyIncome;
+
+    public IncomeSource(String userId, CashFlowName name, Money monthlyIncome) {
+        this.userId = Objects.requireNonNull(userId);
+        this.name = Objects.requireNonNull(name);
+        this.monthlyIncome = Objects.requireNonNull(monthlyIncome);
+    }
 }
