83e9f9a9e4bdae97378b413bb18a8ab7a72e6270
Alexey Korotin <sx12fx@gmail.com>
2024-02-11 16:48:08 +0300

    fix: add id constructors

--- backend/src/main/java/org/itmo/practice/pfm/domain/cashflow/IncomeSource.java
+++ backend/src/main/java/org/itmo/practice/pfm/domain/cashflow/IncomeSource.java
@@ -3,6 +3,7 @@ package org.itmo.practice.pfm.domain.cashflow;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.itmo.practice.pfm.domain.funds.Money;
 
@@ -10,6 +11,7 @@ import java.util.Objects;
 import java.util.UUID;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EqualsAndHashCode(of = {"id"})
 public class IncomeSource {
@@ -29,5 +31,10 @@ public class IncomeSource {
         this.userId = Objects.requireNonNull(userId);
         this.name = Objects.requireNonNull(name);
         this.monthlyIncome = Objects.requireNonNull(monthlyIncome);
+    }
+
+    public IncomeSource(UUID id, String userId, CashFlowName name, Money monthlyIncome) {
+        this(userId, name, monthlyIncome);
+        this.id = Objects.requireNonNull(id);
     }
 }
