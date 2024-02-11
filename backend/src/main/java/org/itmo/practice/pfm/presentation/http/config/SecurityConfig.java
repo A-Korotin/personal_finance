4d8a0ec356b5c129e68ebe8868f5f4be7368ceff
Alexey Korotin <sx12fx@gmail.com>
2024-02-11 11:45:40 +0300

    ref: add http presentation package

--- backend/src/main/java/org/itmo/practice/pfm/presentation/http/config/SecurityConfig.java
+++ backend/src/main/java/org/itmo/practice/pfm/presentation/http/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package org.itmo.practice.pfm.presentation.config;
+package org.itmo.practice.pfm.presentation.http.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
