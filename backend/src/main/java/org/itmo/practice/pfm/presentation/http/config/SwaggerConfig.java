ba1b374f95b3fe8471d34602832358b66d9a743a
Alexey Korotin <sx12fx@gmail.com>
2024-02-11 11:45:40 +0300

    ref: add http presentation package

--- backend/src/main/java/org/itmo/practice/pfm/presentation/http/config/SwaggerConfig.java
+++ backend/src/main/java/org/itmo/practice/pfm/presentation/http/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package org.itmo.practice.pfm.presentation.config;
+package org.itmo.practice.pfm.presentation.http.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
